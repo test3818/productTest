1d766c26031ef2f1db587d2f58cef8b6a2d5802b
Bryan Villafuerte <bryanvillafuerte59@gmail.com>
2019-10-10 14:40:47 -0700

    boilerplate w d3

---
+++ src/client/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Visualizer from './components/Visualizer'
+
+
+const App = () => {
+  return (
+    <div>
+      <Visualizer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/main.ts
@@ -0,0 +1,16 @@
+'use strict';
+const { app, BrowserWindow } = require('electron');
+const electron = require('electron')
+
+console.log(__dirname);
+app.on('ready', () => {
+  // This creates a window on startup
+  const window = new BrowserWindow({ width: 800, height: 600 });
+
+  // This hides the default menu that comes with the browser window
+  // window.setMenuBarVisibility(false);
+
+  // This loads a website to display
+  // console.log(__dirname);
+  window.loadURL(`file://${__dirname}/index.html`);
+});
